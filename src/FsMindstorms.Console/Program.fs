f85bf3dbed728cda48b1903ab447756284068589
Atle <atle.rudshaug@gmail.com>
2019-12-10 20:08:02 +0100

    Make F# console app compile

--- src/FsMindstorms.Console/Program.fs
+++ src/FsMindstorms.Console/Program.fs
@@ -11,26 +11,22 @@ open FsMindstorms.Domain.Builder
 let main argv =
 
     let comm = UsbCommunication("EV3OLAV");
-    let device = comm.FindBrick()
-    printfn "I found this thing: %A" device
-    let blah = device.GetMaxInputReportLength()
-    let stream = device.Open()
-    // let responseManager = ResponseManager();
-    // let brick = Brick(comm, responseManager);
+    let responseManager = ResponseManager();
+    let brick = Brick(comm, responseManager);
 
-    // let connection = brick.Connect().Subscribe()
+    let connection = brick.Connect().Subscribe()
 
-    // let snippet = mindstorms {
-    //     // Turn (Motor OutputPort.A) With Power 50
-    //     // Turn (Motors [ OutputPort.A; OutputPort.B ]) With Power 50
-    //      TurnForTime 1000u (Motors [ OutputPort.A; OutputPort.B ]) With Power 50 Then Break
-    //     // Step (Motors [ OutputPort.A; OutputPort.B ]) For 180u Steps With Power 50 Then Coast
-    //     // StepSync (Motors [ OutputPort.A; OutputPort.B ]) For 180u Steps With Power 50 And TurnRatio 42s Then Coast
-    //     // Start (Motor OutputPort.A)
-    //     }
+    let snippet = mindstorms {
+        // Turn (Motor OutputPort.A) With Power 50
+        // Turn (Motors [ OutputPort.A; OutputPort.B ]) With Power 50
+         TurnForTime 1000u (Motors [ OutputPort.A; OutputPort.B ]) With Power 50 Then Break
+        // Step (Motors [ OutputPort.A; OutputPort.B ]) For 180u Steps With Power 50 Then Coast
+        // StepSync (Motors [ OutputPort.A; OutputPort.B ]) For 180u Steps With Power 50 And TurnRatio 42s Then Coast
+        // Start (Motor OutputPort.A)
+        }
 
-    // let cmd = createCommand brick snippet
-    // brick.SendCommandAsync(cmd) |> ignore
+    let cmd = createCommand brick snippet
+    brick.SendCommandAsync(cmd) |> ignore
 
     // // let x =
     //    brick
